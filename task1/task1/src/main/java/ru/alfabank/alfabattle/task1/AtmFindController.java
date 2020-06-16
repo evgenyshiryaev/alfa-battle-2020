07770876f58aaf688f379deb0a99d28a9bb7fa5d
Evgeny Shiryaev <arpmipg@gmail.com>
2020-06-16 20:07:16 +0300

    cert is ok now

--- task1/task1/src/main/java/ru/alfabank/alfabattle/task1/AtmFindController.java
+++ task1/task1/src/main/java/ru/alfabank/alfabattle/task1/AtmFindController.java
@@ -1,18 +1,33 @@
 package ru.alfabank.alfabattle.task1;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyStore;
+import java.util.stream.Collectors;
 
 import javax.net.ssl.SSLContext;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.util.ResourceUtils;
@@ -33,11 +48,7 @@ public class AtmFindController {
     @GetMapping
     public String getNearestAtm() {
         try {
-//            RestTemplate restTemplate = new RestTemplate();
-            RestTemplate restTemplate = getRestTemplate();
-            ResponseEntity<String> response = restTemplate.getForEntity(
-                    "https://apiws.alfabank.ru/alfabank/alfadevportal/atm-service/atms", String.class);
-            log.info(response.toString());
+            test1();
         } catch (Exception e) {
             log.error("Error", e);
         }
@@ -46,47 +57,41 @@ public class AtmFindController {
     }
 
 
-    private RestTemplate getRestTemplate() throws Exception {
-        char[] password = "fuckoff".toCharArray();
+    private void test1() throws Exception {
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        char[] password = "password".toCharArray();
+        keyStore.load(new FileInputStream(ResourceUtils.getFile("classpath:alfabattle.jks")), password);
 
-        SSLContext sslContext = SSLContextBuilder.create()
-                .loadKeyMaterial(keyStore("classpath:apidevelopers.jks", password), password)
-                .loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
-
-        HttpClient client = HttpClients.custom().setSSLContext(sslContext).build();
-        return builder
-                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(client))
+        SSLContext sslContext = SSLContexts.custom()
+                .loadKeyMaterial(keyStore, "password".toCharArray())
+                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
                 .build();
-    }
 
-     private KeyStore keyStore(String file, char[] password) throws Exception {
-        KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        File key = ResourceUtils.getFile(file);
-        try (InputStream in = new FileInputStream(key)) {
-            keyStore.load(in, password);
-        }
-        return keyStore;
-    }
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLContext(sslContext)
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
 
+        HttpUriRequest request = RequestBuilder.get()
+                .setUri(URI.create("https://apiws.alfabank.ru/alfabank/alfadevportal/atm-service/atms"))
+                .setHeader("accept", "application/json")
+                .setHeader("x-ibm-client-id", "d79fc084-69ec-4d8e-9dfd-daf8b5d04ef9")
+                .build();
+        CloseableHttpResponse response = httpClient.execute(request);
+        log.info(response.toString());
+        String text = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
+        log.info(text);
 
-//    private RestTemplate restTemplate() {
-//        KeyStore clientStore = KeyStore.getInstance("PKCS12");
-//        clientStore.load(new FileInputStream("/path/to/certfile"),
-//                "certpassword".toCharArray());
+//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+//        requestFactory.setHttpClient(httpClient);
 //
-//        SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
-//        sslContextBuilder.useProtocol("TLS");
-//        sslContextBuilder.loadKeyMaterial(clientStore, "certpassword".toCharArray());
-//        sslContextBuilder.loadTrustMaterial(new TrustSelfSignedStrategy());
-//     
-//        SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContextBuilder.build());
-//        CloseableHttpClient httpClient = HttpClients.custom()
-//                .setSSLSocketFactory(sslConnectionSocketFactory)
-//                .build();
-//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-//        requestFactory.setConnectTimeout(10000); // 10 seconds
-//        requestFactory.setReadTimeout(10000); // 10 seconds
-//        return new RestTemplate(requestFactory);
-//    }
+//        ResponseEntity<String> response = new RestTemplate(requestFactory).exchange(
+//                URI.create("https://apiws.alfabank.ru/alfabank/alfadevportal/atm-service/atms"),
+//                HttpMethod.GET, null, String.class);
+//        log.info(response.toString());
+    }
 
 }
