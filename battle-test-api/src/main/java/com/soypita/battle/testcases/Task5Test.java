cf94650816fb47de325fdacf1b9a124b87aa3b63
Evgeny Shiryaev <arpmipg@gmail.com>
2020-06-23 18:54:53 +0300

    test cases empty class

---
+++ battle-test-api/src/main/java/com/soypita/battle/testcases/Task5Test.java
@@ -0,0 +1,22 @@
+package com.soypita.battle.testcases;
+
+import org.junit.jupiter.api.Test;
+
+//Total tests = TODO
+//Points per test = TODO
+public class Task5Test extends BaseTest {
+
+    // 1
+    // tests = 6, points = 24
+
+    @Test
+    public void getByIdSampleFoundTest(String host) {
+        // TODO
+    }
+
+    // TODO
+
+
+    
+
+}
