b2920053def1a941b54f7c1618c807cf5ef1949f
Evgeny Shiryaev <arpmipg@gmail.com>
2020-06-25 19:29:17 +0300

    add swagger

--- Task3/src/main/java/ru/alfa/task3/TaskApplication.java
+++ Task3/src/main/java/ru/alfa/task3/TaskApplication.java
@@ -2,14 +2,13 @@ package ru.alfa.task3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@EnableScheduling
 @SpringBootApplication
 public class TaskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskApplication.class, args);
     }
+
 }
