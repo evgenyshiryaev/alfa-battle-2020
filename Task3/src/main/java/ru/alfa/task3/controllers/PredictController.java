8d76c9a704bc8f161a148994a0ac1f7ce883ec81
Evgeny Shiryaev <arpmipg@gmail.com>
2020-06-25 19:48:32 +0300

    add 404 remove classes

--- Task3/src/main/java/ru/alfa/task3/controllers/PredictController.java
+++ Task3/src/main/java/ru/alfa/task3/controllers/PredictController.java
@@ -1,13 +1,20 @@
 package ru.alfa.task3.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import ru.alfa.task3.dto.BranchesWithPredicting;
 import ru.alfa.task3.dto.DataTimeForModel;
+import ru.alfa.task3.dto.ErrorResponse;
+import ru.alfa.task3.entity.Branches;
 import ru.alfa.task3.services.BranchesService;
 import ru.alfa.task3.services.DataAnalystService;
 
@@ -24,15 +31,23 @@ public class PredictController {
     }
 
     @GetMapping(value = "/branches/{id}/predict", produces = MediaType.APPLICATION_JSON_VALUE)
-    public BranchesWithPredicting getBranchesWithPredicting(@PathVariable Long id, @RequestParam Integer dayOgWeek, @RequestParam Integer hourOfDay) {
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "OK", response = BranchesWithPredicting.class),
+        @ApiResponse(code = 404, message = "NOT FOUND", response = ErrorResponse.class)})
+    public ResponseEntity<?> getBranchesWithPredicting(@PathVariable Long id, @RequestParam Integer dayOgWeek, @RequestParam Integer hourOfDay) {
+        Branches branches = branchesService.findById(id);
+        if (branches == null) {
+            return new ResponseEntity<>(new ErrorResponse("branch not found"), HttpStatus.NOT_FOUND);
+        }
 
-        BranchesWithPredicting branchesWithPredicting = new BranchesWithPredicting(branchesService.findById(id));
+        BranchesWithPredicting branchesWithPredicting = new BranchesWithPredicting(branches);
         DataTimeForModel dataTimeForModel = new DataTimeForModel(id, dayOgWeek, hourOfDay);
 
         branchesWithPredicting.setPredicting(dataAnalystService.getModel().get(dataTimeForModel));
         branchesWithPredicting.setHourOfDay(hourOfDay);
         branchesWithPredicting.setDayOfWeek(dayOgWeek);
 
-        return branchesWithPredicting;
+        return ResponseEntity.ok(branchesWithPredicting);
     }
+
 }
