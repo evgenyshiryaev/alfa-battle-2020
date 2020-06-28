ea6291b4c0e0019e72792f1ecc64e0c564a2ad47
Evgeny Shiryaev <arpmipg@gmail.com>
2020-06-29 02:14:49 +0300

    Revert springfox version

--- task5/src/main/java/com/x5/promo/ApplicationConfiguration.java
+++ task5/src/main/java/com/x5/promo/ApplicationConfiguration.java
@@ -18,7 +18,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
@@ -36,7 +36,7 @@ public class ApplicationConfiguration {
   }
 
 
-  @EnableSwagger2WebMvc
+  @EnableSwagger2
   @Configuration
   public static class SwaggerConfiguration {
     @Bean
